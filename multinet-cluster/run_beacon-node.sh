dae6c2ac9edb00b19df756a6805eb844e9cc245e
Patryk Krakos <patryk.krakos@m8b.pl>
2021-03-04 16:39:18 +0100

    Synchronization changes

--- multinet-cluster/run_beacon-node.sh
+++ multinet-cluster/run_beacon-node.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Running prysm (2)";
+echo "Running prysm beacon-node";
 
 mkdir /root/multinet/repo/data/common;
 
@@ -16,26 +16,15 @@ while  [ ! -f /root/multinet/repo/data/common/genesis.ssz ]; do
   sleep 5;
 done
 
-if [ "$MULTINET_POD_NAME" == "prysm-0" ]; then
-  ./prysm.sh beacon-chain --datadir /tmp/chaindata \
+./prysm.sh beacon-chain --datadir /tmp/chaindata \
   --force-clear-db \
   --interop-genesis-state /root/multinet/repo/data/common/genesis.ssz \
   --interop-eth1data-votes \
+  --verbosity=debug \
   --min-sync-peers=0 \
-  --verbosity=debug \
   --bootstrap-node=enr:-Ku4QFuEmIcEMoNAox0fh1PdJMeYdzruWFI1rNXi0Xk-Bk9pPpetCQSu27X9vUjYj7rS67L6tJaWWgaB-aNx16-0InkBh2F0dG5ldHOIAAAAAAAAAACEZXRoMpD1pf1CAAAAAP__________gmlkgnY0gmlwhAoAAUaJc2VjcDI1NmsxoQKWfbT1atCho149MGMvpgBWUymiOv9QyXYhgYEBZvPBW4N1ZHCCD6A \
   --accept-terms-of-use
-fi
 
-if [ "$MULTINET_POD_NAME" != "prysm-0" ]; then
-  ./prysm.sh beacon-chain --datadir /tmp/chaindata \
-  --force-clear-db \
-  --interop-genesis-state /root/multinet/repo/data/common/genesis.ssz \
-  --interop-eth1data-votes \
-  --verbosity=debug \
-  --bootstrap-node=enr:-Ku4QFuEmIcEMoNAox0fh1PdJMeYdzruWFI1rNXi0Xk-Bk9pPpetCQSu27X9vUjYj7rS67L6tJaWWgaB-aNx16-0InkBh2F0dG5ldHOIAAAAAAAAAACEZXRoMpD1pf1CAAAAAP__________gmlkgnY0gmlwhAoAAUaJc2VjcDI1NmsxoQKWfbT1atCho149MGMvpgBWUymiOv9QyXYhgYEBZvPBW4N1ZHCCD6A \
-  --accept-terms-of-use
-fi
 
 
 
--- multinet-cluster/run_validator.sh
+++ multinet-cluster/run_validator.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+echo 'Running prysm validator';
 echo $MULTINET_POD_IP;
 
 
-  bazel run //validator -- --beacon-rpc-provider localhost:4000 \
+  ./prysm.sh validator --beacon-rpc-provider localhost:4000 \
   --interop-num-validators=500 \
   --interop-start-index=0 \
   --force-clear-db \
