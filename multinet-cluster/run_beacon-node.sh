3bf00ed75228275320487ec2a620ea9ba4a83c28
Patryk Krakos <patryk.krakos@m8b.pl>
2021-03-05 19:48:26 +0100

    External IP for pods

--- multinet-cluster/run_beacon-node.sh
+++ multinet-cluster/run_beacon-node.sh
@@ -5,10 +5,23 @@ echo "Running prysm beacon-node";
 mkdir /root/multinet/repo/data/common;
 
 if [ "$MULTINET_POD_NAME" == "prysm-0" ]; then
+  EXTERNAL_IP=34.91.111.241;
   rm /root/multinet/repo/data/common/genesis.ssz
-  bazel run //tools/genesis-state-gen -- --num-validators=500 \
+  bazel run //tools/genesis-state-gen -- --num-validators=400 \
   --output-ssz=/root/multinet/repo/data/common/genesis.ssz \
   --mainnet-config
+fi
+
+if [ "$MULTINET_POD_NAME" == "prysm-1" ]; then
+  EXTERNAL_IP=34.91.186.254;
+fi
+
+if [ "$MULTINET_POD_NAME" == "prysm-2" ]; then
+  EXTERNAL_IP=34.90.76.16;
+fi
+
+if [ "$MULTINET_POD_NAME" == "prysm-3" ]; then
+  EXTERNAL_IP=35.204.181.122;
 fi
 
 
@@ -23,6 +36,8 @@ done
   --monitoring-host=0.0.0.0 \
   --verbosity=debug \
   --min-sync-peers=0 \
+  --p2p-max-peers=10 \
+  --p2p-host-ip=$EXTERNAL_IP \
   --bootstrap-node=enr:-Ku4QFuEmIcEMoNAox0fh1PdJMeYdzruWFI1rNXi0Xk-Bk9pPpetCQSu27X9vUjYj7rS67L6tJaWWgaB-aNx16-0InkBh2F0dG5ldHOIAAAAAAAAAACEZXRoMpD1pf1CAAAAAP__________gmlkgnY0gmlwhAoAAUaJc2VjcDI1NmsxoQKWfbT1atCho149MGMvpgBWUymiOv9QyXYhgYEBZvPBW4N1ZHCCD6A \
   --accept-terms-of-use
 
--- multinet-cluster/run_validator.sh
+++ multinet-cluster/run_validator.sh
@@ -4,8 +4,34 @@ echo 'Running prysm validator';
 echo $MULTINET_POD_IP;
 
 
+if [ "$MULTINET_POD_NAME" == "prysm-0" ]; then
   ./prysm.sh validator --beacon-rpc-provider localhost:4000 \
-  --interop-num-validators=500 \
+  --interop-num-validators=100 \
   --interop-start-index=0 \
   --force-clear-db \
   --accept-terms-of-use
+fi
+
+if [ "$MULTINET_POD_NAME" == "prysm-1" ]; then
+  ./prysm.sh validator --beacon-rpc-provider localhost:4000 \
+  --interop-num-validators=100 \
+  --interop-start-index=100 \
+  --force-clear-db \
+  --accept-terms-of-use
+fi
+
+if [ "$MULTINET_POD_NAME" == "prysm-2" ]; then
+  ./prysm.sh validator --beacon-rpc-provider localhost:4000 \
+  --interop-num-validators=100 \
+  --interop-start-index=200 \
+  --force-clear-db \
+  --accept-terms-of-use
+fi
+
+if [ "$MULTINET_POD_NAME" == "prysm-3" ]; then
+  ./prysm.sh validator --beacon-rpc-provider localhost:4000 \
+  --interop-num-validators=100 \
+  --interop-start-index=300 \
+  --force-clear-db \
+  --accept-terms-of-use
+fi
