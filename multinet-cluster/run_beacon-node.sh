512ed8d7fb7f070edcdea1a0e509ba77da32ca1d
Patryk Krakos <patryk.krakos@m8b.pl>
2021-05-11 10:56:00 +0200

    Staging namespace

--- multinet-cluster/run_beacon-node.sh
+++ multinet-cluster/run_beacon-node.sh
@@ -35,7 +35,7 @@ chmod +x ./beacon-chain.run &&
   --force-clear-db \
   --datadir /tmp/chaindata \
   --chain-config-file=/root/multinet/repo/data/common/chain-config.yaml \
-  --bootstrap-node="enr:-Ku4QOoNjcQp4g7jd8IziJ_pL29CWbC_UDJ7iC7WNOPo26L4VtSS_F8eLcZ3jQIl7YfTkkgJK85bQ4zYFH_maL3XrmEBh2F0dG5ldHOIAAAAAAAAAACEZXRoMpD1pf1CAAAAAP__________gmlkgnY0gmlwhAoAAg-Jc2VjcDI1NmsxoQKWfbT1atCho149MGMvpgBWUymiOv9QyXYhgYEBZvPBW4N1ZHCCD6A" \
+  --bootstrap-node="enr:-Ku4QP5Qaw96Pstr2IX4BG1D4cPfWqDoSLppdSaZ3mdqvbVoOMAjIDDhRRfIJJFEs4i70dqbOCyBAZrXNn-f-ZVerugBh2F0dG5ldHOIAAAAAAAAAACEZXRoMpD1pf1CAAAAAP__________gmlkgnY0gmlwhAoACRCJc2VjcDI1NmsxoQKWfbT1atCho149MGMvpgBWUymiOv9QyXYhgYEBZvPBW4N1ZHCCD6A" \
   --http-web3provider=http://127.0.0.1:8545 \
   --deposit-contract=0x000000000000000000000000000000000000cafe \
   --contract-deployment-block=0 \
@@ -44,6 +44,6 @@ chmod +x ./beacon-chain.run &&
   --verbosity=debug \
   --min-sync-peers=0 \
   --p2p-max-peers=10 \
-  --p2p-host-ip=$EXTERNAL_IP \
+  --p2p-host-ip=$MULTINET_POD_IP \
   
   
