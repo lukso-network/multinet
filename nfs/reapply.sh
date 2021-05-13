35c487437a35d95d4170304d2a5352477b95cc85
Patryk Krakos <patryk.krakos@m8b.pl>
2021-05-13 17:43:57 +0200

    Fixed volume issue

--- nfs/reapply.sh
+++ nfs/reapply.sh
@@ -2,14 +2,14 @@
 #WARNING: run this from root of the project
 
 # delete old nfs deployment
-kubectl delete -f $PWD/nfs/deployment-data.yaml --namespace test
-kubectl delete -f $PWD/nfs/deployment-deposit.yaml --namespace test
-kubectl delete -f $PWD/nfs/service-data.yaml --namespace test
-kubectl delete -f $PWD/nfs/service-deposit.yaml --namespace test
+kubectl delete -f $PWD/nfs/deployment-data.yaml --namespace dev
+kubectl delete -f $PWD/nfs/deployment-deposit.yaml --namespace dev
+kubectl delete -f $PWD/nfs/service-data.yaml --namespace dev
+kubectl delete -f $PWD/nfs/service-deposit.yaml --namespace dev
 
 #apply new nfs deployment
-kubectl apply -f $PWD/nfs/deployment-data.yaml --namespace test
-kubectl apply -f $PWD/nfs/deployment-deposit.yaml --namespace test
-kubectl apply -f $PWD/nfs/service-data.yaml --namespace test
-kubectl apply -f $PWD/nfs/service-deposit.yaml --namespace test
+kubectl apply -f $PWD/nfs/deployment-data.yaml --namespace dev
+kubectl apply -f $PWD/nfs/deployment-deposit.yaml --namespace dev
+kubectl apply -f $PWD/nfs/service-data.yaml --namespace dev
+kubectl apply -f $PWD/nfs/service-deposit.yaml --namespace dev
 
