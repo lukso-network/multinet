e2a651fe4cd53764ad0d0596cba80c7b79ab8316
Patryk Krakos <patryk.krakos@m8b.pl>
2021-05-18 17:42:22 +0200

    Fixing

--- nfs/reapply.sh
+++ nfs/reapply.sh
@@ -2,14 +2,14 @@
 #WARNING: run this from root of the project
 
 # delete old nfs deployment
-kubectl delete -f $PWD/nfs/deployment-data.yaml --namespace test
-kubectl delete -f $PWD/nfs/deployment-deposit.yaml --namespace test
-kubectl delete -f $PWD/nfs/service-data.yaml --namespace test
-kubectl delete -f $PWD/nfs/service-deposit.yaml --namespace test
+kubectl delete -f $PWD/nfs/deployment-data.yaml --namespace staging
+kubectl delete -f $PWD/nfs/deployment-deposit.yaml --namespace staging
+kubectl delete -f $PWD/nfs/service-data.yaml --namespace staging
+kubectl delete -f $PWD/nfs/service-deposit.yaml --namespace staging
 
 #apply new nfs deployment
-kubectl apply -f $PWD/nfs/deployment-data.yaml --namespace test
-kubectl apply -f $PWD/nfs/deployment-deposit.yaml --namespace test
-kubectl apply -f $PWD/nfs/service-data.yaml --namespace test
-kubectl apply -f $PWD/nfs/service-deposit.yaml --namespace test
+kubectl apply -f $PWD/nfs/deployment-data.yaml --namespace staging
+kubectl apply -f $PWD/nfs/deployment-deposit.yaml --namespace staging
+kubectl apply -f $PWD/nfs/service-data.yaml --namespace staging
+kubectl apply -f $PWD/nfs/service-deposit.yaml --namespace staging
 
