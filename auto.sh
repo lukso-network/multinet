f74aae0e410a897dc41296a53f68b6e26fdfaa04
Patryk Krakos <patryk.krakos@m8b.pl>
2021-05-31 15:56:35 +0200

    Modifed auto script for GH actions

--- auto.sh
+++ auto.sh
@@ -7,7 +7,7 @@ GENESIS_START=$(echo "$(date +%s)"+180 | bc) &&
 --mainnet-config \
 --num-validators=2000 \
 --genesis-time="$GENESIS_START" &&
-kubectl cp /tmp/genesis.ssz  prysm-0:/root/multinet/repo/data/common/genesis.ssz \
+kubectl cp /tmp/genesis.ssz  prysm-0:/root/multinet/repo/data/common/genesis.ssz --dry-run \
 -c vanguard -n $NAMESPACE &&
 
 if [ "$VANGUARD" ]; then
@@ -29,8 +29,8 @@ fi
 yq eval '.ETH_2_GENESIS_TIME = '"$GENESIS_START" multinet-cluster/values.yaml -i &&
 yq eval '.ETH_2_GENESIS_TIME = '"$GENESIS_START" multinet-cluster/values.yaml -i &&
 yq eval '.MIN_GENESIS_TIME = '$GENESIS_START chain-config.yaml -i &&
-kubectl cp ./chain-config.yaml prysm-0:/root/multinet/repo/data/common/chain-config.yaml -c vanguard -n $NAMESPACE &&
+kubectl cp ./chain-config.yaml prysm-0:/root/multinet/repo/data/common/chain-config.yaml -c vanguard -n $NAMESPACE --dry-run &&
 helm upgrade -f multinet-cluster/values.yaml eth20 ./multinet-cluster/ --namespace "$NAMESPACE" --dry-run &&
-kubectl delete pods prysm-0 prysm-1 prysm-2 prysm-3 --namespace "$NAMESPACE" &&
+kubectl delete pods prysm-0 prysm-1 prysm-2 prysm-3 --namespace "$NAMESPACE" --dry-run &&
 echo "$GENESIS_START"
 #kubectl get pods -n "$NAMESPACE" -w
---
+++ manual_upgrade.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#echo "Not ready! Please do not use" &&
+#exit;
+
+echo $NAMESPACE;
+GENESIS_START=$(echo "$(date +%s)"+180 | bc) &&
+./genesis-state-gen --output-ssz=/tmp/genesis.ssz \
+--mainnet-config \
+--num-validators=2000 \
+--genesis-time="$GENESIS_START" &&
+kubectl cp /tmp/genesis.ssz  prysm-0:/root/multinet/repo/data/common/genesis.ssz \
+-c vanguard -n $NAMESPACE &&
+yq eval '.ETH_2_GENESIS_TIME = '"$GENESIS_START" multinet-cluster/values.yaml -i &&
+yq eval '.MIN_GENESIS_TIME = '$GENESIS_START chain-config.yaml -i &&
+kubectl cp ./chain-config.yaml prysm-0:/root/multinet/repo/data/common/chain-config.yaml -c vanguard -n $NAMESPACE &&
+helm upgrade -f multinet-cluster/values.yaml eth20 ./multinet-cluster/ --namespace "$NAMESPACE" &&
+kubectl delete pods prysm-0 prysm-1 prysm-2 prysm-3 --namespace "$NAMESPACE" &&
+echo "$GENESIS_START"
+#kubectl get pods -n "$NAMESPACE" -w
