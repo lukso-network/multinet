e31561edfe6a3a16182e6a916b2b59722785d379
Patryk Krakos <patryk.krakos@m8b.pl>
2021-05-31 16:02:50 +0200

    Modifed auto script for GH actions

--- auto.sh
+++ auto.sh
@@ -7,8 +7,8 @@ GENESIS_START=$(echo "$(date +%s)"+180 | bc) &&
 --mainnet-config \
 --num-validators=2000 \
 --genesis-time="$GENESIS_START" &&
-kubectl cp /tmp/genesis.ssz  prysm-0:/root/multinet/repo/data/common/genesis.ssz \
--c vanguard -n $NAMESPACE --dry-run &&
+#kubectl cp /tmp/genesis.ssz  prysm-0:/root/multinet/repo/data/common/genesis.ssz \
+#-c vanguard -n $NAMESPACE &&
 
 if [ "$VANGUARD" ]; then
   yq eval '.VANGUARD_GH_TAG = '"$VANGUARD" multinet-cluster/values.yaml -i
@@ -27,9 +27,8 @@ if [ "$ORCHESTRATOR" ]; then
 fi
 
 yq eval '.ETH_2_GENESIS_TIME = '"$GENESIS_START" multinet-cluster/values.yaml -i &&
-yq eval '.ETH_2_GENESIS_TIME = '"$GENESIS_START" multinet-cluster/values.yaml -i &&
 yq eval '.MIN_GENESIS_TIME = '$GENESIS_START chain-config.yaml -i &&
-kubectl cp ./chain-config.yaml prysm-0:/root/multinet/repo/data/common/chain-config.yaml -c vanguard -n $NAMESPACE --dry-run &&
+#kubectl cp ./chain-config.yaml prysm-0:/root/multinet/repo/data/common/chain-config.yaml -c vanguard -n $NAMESPACE &&
 helm upgrade -f multinet-cluster/values.yaml eth20 ./multinet-cluster/ --namespace "$NAMESPACE" --dry-run &&
 kubectl delete pods prysm-0 prysm-1 prysm-2 prysm-3 --namespace "$NAMESPACE" --dry-run &&
 echo "$GENESIS_START"
