3a64c797eaf767e80693af6a7bc5a1430696f2cf
Patryk Krakos <patryk.krakos@m8b.pl>
2021-06-03 11:13:04 +0200

    Deleted dry run

--- auto.sh
+++ auto.sh
@@ -7,14 +7,14 @@ GENESIS_START=$(echo "$(date +%s)"+180 | bc) &&
 --mainnet-config \
 --num-validators=2000 \
 --genesis-time="$GENESIS_START" &&
-#kubectl cp /tmp/genesis.ssz  prysm-0:/root/multinet/repo/data/common/genesis.ssz \
-#-c vanguard -n $NAMESPACE &&
+kubectl cp /tmp/genesis.ssz  prysm-0:/root/multinet/repo/data/common/genesis.ssz \
+-c vanguard -n $NAMESPACE &&
 
 # Replacing values in "values.yaml"
 yq eval '.'$REPOSITORY'_GH_TAG = "'$REPOSITORY_TAG'"' multinet-cluster/values.yaml -i
 yq eval '.ETH_2_GENESIS_TIME = '"$GENESIS_START" multinet-cluster/values.yaml -i &&
 yq eval '.MIN_GENESIS_TIME = '$GENESIS_START chain-config.yaml -i &&
-#kubectl cp ./chain-config.yaml prysm-0:/root/multinet/repo/data/common/chain-config.yaml -c vanguard -n $NAMESPACE &&
-helm upgrade -f multinet-cluster/values.yaml eth20 ./multinet-cluster/ --namespace "$NAMESPACE" --dry-run &&
-kubectl delete pods prysm-0 prysm-1 prysm-2 prysm-3 --namespace "$NAMESPACE" --dry-run &&
+kubectl cp ./chain-config.yaml prysm-0:/root/multinet/repo/data/common/chain-config.yaml -c vanguard -n $NAMESPACE &&
+helm upgrade -f multinet-cluster/values.yaml eth20 ./multinet-cluster/ --namespace "$NAMESPACE" &&
+kubectl delete pods prysm-0 prysm-1 prysm-2 prysm-3 --namespace "$NAMESPACE" &&
 echo "Done"
