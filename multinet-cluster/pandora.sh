eb81d08c1f7c3b6425c7dd261127ee6d1c033743
mxmar <macht4tech@gmail.com>
2021-06-30 17:28:13 +0200

    Deposit contract debugging - reverted genesis file name
    Signed-off-by: mxmar <macht4tech@gmail.com>

--- multinet-cluster/pandora.sh
+++ multinet-cluster/pandora.sh
@@ -2,7 +2,7 @@
 apt update &&
 yes | apt install wget &&
 wget https://github.com/lukso-network/pandora-execution-engine/releases/download/"$PANDORA_GH_TAG"/geth -O ./geth &&
-wget https://storage.googleapis.com/l16-common/pandora/pandora_genesis_matt.json -O ./genesis.json &&
+wget https://storage.googleapis.com/l16-common/pandora/pandora_genesis3.json -O ./genesis.json &&
 
 chmod +x ./geth &&
 rm -rf ./pandora &&
