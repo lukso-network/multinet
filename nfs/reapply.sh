b412614fada59d9ae54a9bcf545a39bd271f7fee
Patryk Krakos <patryk.krakos@m8b.pl>
2021-07-12 14:31:08 +0200

    Changed namespace

--- nfs/reapply.sh
+++ nfs/reapply.sh
@@ -2,14 +2,14 @@
 #WARNING: run this from root of the project
 
 # delete old nfs deployment
-kubectl delete -f $PWD/nfs/deployment-data.yaml --namespace staging
-kubectl delete -f $PWD/nfs/deployment-deposit.yaml --namespace staging
-kubectl delete -f $PWD/nfs/service-data.yaml --namespace staging
-kubectl delete -f $PWD/nfs/service-deposit.yaml --namespace staging
+kubectl delete -f $PWD/nfs/deployment-data.yaml --namespace closed-testnet
+kubectl delete -f $PWD/nfs/deployment-deposit.yaml --namespace closed-testnet
+kubectl delete -f $PWD/nfs/service-data.yaml --namespace closed-testnet
+kubectl delete -f $PWD/nfs/service-deposit.yaml --namespace closed-testnet
 
 #apply new nfs deployment
-kubectl apply -f $PWD/nfs/deployment-data.yaml --namespace staging
-kubectl apply -f $PWD/nfs/deployment-deposit.yaml --namespace staging
-kubectl apply -f $PWD/nfs/service-data.yaml --namespace staging
-kubectl apply -f $PWD/nfs/service-deposit.yaml --namespace staging
+kubectl apply -f $PWD/nfs/deployment-data.yaml --namespace closed-testnet
+kubectl apply -f $PWD/nfs/deployment-deposit.yaml --namespace closed-testnet
+kubectl apply -f $PWD/nfs/service-data.yaml --namespace closed-testnet
+kubectl apply -f $PWD/nfs/service-deposit.yaml --namespace closed-testnet
 
