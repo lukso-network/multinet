5cd3ee9a94b71958f2931b4de5460a1d3de97c2d
Patryk Krakos <patryk.krakos@m8b.pl>
2021-07-13 18:31:53 +0200

    Uploaded the validators

--- multinet-cluster/run_validator.sh
+++ multinet-cluster/run_validator.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+apt update &&
+yes | apt install wget &&
 if [ "$MULTINET_POD_NAME" == "prysm-4" ]; then
   sleep infinity;
 fi
@@ -29,15 +30,26 @@ while  [ ! -f /root/multinet/repo/data/common/chain-config.yaml ]; do
   sleep 5;
 done
 
+echo "Waiting for validator keys";
+while  [ ! -f /root/multinet/repo/deposits/validator_keys/deposit_data-1626173952.json ]; do
+  sleep 5;
+done
+
 chmod +x ./validator.run &&
+
+./validator.run accounts import \
+  --accept-terms-of-use \
+  --wallet-dir=/root/multinet/repo/wallet \
+  --wallet-password-file=/root/multinet/repo/deposits/password.txt \
+  --keys-dir=/root/multinet/repo/deposits/validator_keys \
+  --account-password-file=/root/multinet/repo/deposits/password.txt &&
 
 ./validator.run \
   --accept-terms-of-use \
   --beacon-rpc-provider localhost:4000 \
   --chain-config-file=/root/multinet/repo/data/common/chain-config.yaml \
-  --interop-num-validators=500 \
-  --interop-start-index="$INDEX" \
   --force-clear-db \
   --verbosity=debug \
   --pandora-http-provider=http://127.0.0.1:8545 \
-  --lukso-network
+  --lukso-network \
+  --wallet-dir=/root/multinet/repo/wallet
