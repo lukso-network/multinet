c45537618d417c14a1a0e29a921d9fc7d3ade6e2
mxmar <macht4tech@gmail.com>
2021-07-16 18:43:53 +0200

    Namespace: closed-testnet - final configuration with incorrect validators length 31 vs 29;
    Signed-off-by: mxmar <macht4tech@gmail.com>

--- auto.sh
+++ auto.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
 echo $NAMESPACE;
-GENESIS_START=$(echo "$(date +%s)"+150 | bc) &&
-./genesis-state-gen --output-ssz=/tmp/genesis.ssz \
---deposit-json-file ./deposit_data.json \
---mainnet-config \
---genesis-time="$GENESIS_START" &&
-kubectl cp /tmp/genesis.ssz  prysm-0:/root/multinet/repo/data/common/genesis.ssz \
+kubectl cp ./vanguard_private_testnet_genesis.ssz prysm-0:/root/multinet/repo/data/common/genesis.ssz \
 -c pandora -n $NAMESPACE &&
-yq eval '.ETH_2_GENESIS_TIME = '"$GENESIS_START" multinet-cluster/values.yaml -i &&
-yq eval '.MIN_GENESIS_TIME = '$GENESIS_START chain-config.yaml -i &&
 kubectl cp ./chain-config.yaml prysm-0:/root/multinet/repo/data/common/chain-config.yaml -c pandora -n $NAMESPACE &&
 helm upgrade -f multinet-cluster/values.yaml eth20 ./multinet-cluster/ --namespace "$NAMESPACE" &&
 kubectl delete pods prysm-0 prysm-1 prysm-2 prysm-3 --namespace "$NAMESPACE" &&
