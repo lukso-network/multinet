529ca64c91ff856b76dcaa79768fe24b30c8b9ca
mxmar <macht4tech@gmail.com>
2021-07-19 15:23:08 +0200

    Namespace: dev-matt - pandora bootnode as a geth;
    Signed-off-by: mxmar <macht4tech@gmail.com>

--- multinet-cluster/run_pandora-bootnode.sh
+++ multinet-cluster/run_pandora-bootnode.sh
@@ -4,9 +4,13 @@ echo "Running pandora bootnode";
 
 apt update &&
 yes | apt install wget &&
+wget https://github.com/lukso-network/pandora-execution-engine/releases/download/"$PANDORA_GH_TAG"/geth -O ./geth &&
+wget https://storage.googleapis.com/l16-common/pandora/pandora_private_testnet_genesis.json -O ./genesis.json &&
 
-wget https://storage.googleapis.com/l16-common/pandora/bootnode -O ./bootnode &&
-chmod +x ./bootnode
-./bootnode -nodekeyhex 5ad02bf648b850a44829d9b38976b1b01ddd000fc4c6099ff54aebf4ea07b867
+chmod +x ./geth &&
+./geth --datadir /blockchainData/pandora-bootnode/"$ETH_2_GENESIS_TIME" init ./genesis.json &&
 
-
+./geth --datadir /blockchainData/pandora-bootnode/"$ETH_2_GENESIS_TIME" \
+--networkid 4004181 \
+--verbosity 6 \
+--nat extip:$MULTINET_POD_IP
