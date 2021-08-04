de81577b32e4938ea5555c1a091775e9798785b0
Patryk Krakos <patryk.krakos@m8b.pl>
2021-08-04 13:39:58 +0200

    Service for ethstats

--- multinet-cluster/run_beaconchain-explorer.sh
+++ multinet-cluster/run_beaconchain-explorer.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 apt update;
-yes | apt install wget;
+yes | apt install wget git;
 
 mkdir /explorer;
 cd /explorer;
+git clone https://github.com/gobitfly/eth2-beaconchain-explorer;
+cd eth2-beaconchain-explorer;
 
-wget https://storage.googleapis.com/l16-common/beaconchain-explorer/explorer;
+
+wget https://storage.googleapis.com/l16-common/beaconchain-explorer/bin.zip;
 wget https://storage.googleapis.com/l16-common/beaconchain-explorer/vanguard-config.yml;
 wget https://storage.googleapis.com/l16-common/beaconchain-explorer/config.yml;
+
+tar -xzvf bin.zip;
 
 chmod +x explorer;
 ./explorer --config config.yml
