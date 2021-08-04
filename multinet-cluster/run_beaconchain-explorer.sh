3fbd7af63116c3bd89f354b2268db2cbd1835cb5
Patryk Krakos <patryk.krakos@m8b.pl>
2021-08-04 16:27:38 +0200

    Fixed service for beacon chain explorer

--- multinet-cluster/run_beaconchain-explorer.sh
+++ multinet-cluster/run_beaconchain-explorer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt update;
-yes | apt install wget git unzip;
+yes | apt install wget git unzip postgresql-client-common postgresql-client-12;
 
 mkdir /explorer;
 cd /explorer;
@@ -11,13 +11,13 @@ cd eth2-beaconchain-explorer;
 
 wget https://storage.googleapis.com/l16-common/beaconchain-explorer/bin.zip;
 wget https://storage.googleapis.com/l16-common/beaconchain-explorer/vanguard-config.yml;
-wget https://storage.googleapis.com/l16-common/beaconchain-explorer/config.yml;
-wget https://storage.googleapis.com/l16-common/beaconchain-explorer/tables.sql;
-
-psql -f /src/tables.sql -d db -h 127.0.0.1 -U postgres;
+wget https://storage.googleapis.com/l16-common/beaconchain-explorer/config1.yml;
+#wget https://storage.googleapis.com/l16-common/beaconchain-explorer/tables.sql;
 
 rm -rf ./bin;
 unzip bin.zip;
 
+psql -f ./tables.sql -d db -h 127.0.0.1 -U postgres;
+
 chmod +x ./bin/explorer;
-./bin/explorer --config config.yml
+./bin/explorer --config config1.yml
