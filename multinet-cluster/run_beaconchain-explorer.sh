56baebb64189a87e86c767804489fcfe9a8077ce
Patryk Krakos <patryk.krakos@m8b.pl>
2021-08-20 18:06:46 +0200

    Changed beaconchain-explorer config

--- multinet-cluster/run_beaconchain-explorer.sh
+++ multinet-cluster/run_beaconchain-explorer.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-sleep infinity;
 apt update;
 yes | apt install wget git unzip postgresql-client-common postgresql-client-12;
 
 mkdir /explorer;
 cd /explorer;
 git clone https://github.com/gobitfly/eth2-beaconchain-explorer;
+cp ./config1.yml ./eth2-beaconchain-explorer;
 cd eth2-beaconchain-explorer;
+git checkout c9ef8e810ffb04ad69142421d23a93a17e32cf44;
 
 
 wget https://storage.googleapis.com/l16-common/beaconchain-explorer/bin.zip;
 wget https://storage.googleapis.com/l16-common/beaconchain-explorer/vanguard-config.yml;
-wget https://storage.googleapis.com/l16-common/beaconchain-explorer/config1.yml;
+#wget https://storage.googleapis.com/l16-common/beaconchain-explorer/config1.yml;
 #wget https://storage.googleapis.com/l16-common/beaconchain-explorer/tables.sql;
 
 rm -rf ./bin;
