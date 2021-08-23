33025c81b9c318e5168b4997d5edbde11653b743
Patryk Krakos <patryk.krakos@m8b.pl>
2021-08-23 10:21:00 +0200

    Replace binary for beacon-cahin explorer

--- multinet-cluster/run_beaconchain-explorer.sh
+++ multinet-cluster/run_beaconchain-explorer.sh
@@ -4,19 +4,26 @@ yes | apt install wget git unzip postgresql-client-common postgresql-client-12;
 
 mkdir /explorer;
 cd /explorer;
-git clone https://github.com/gobitfly/eth2-beaconchain-explorer;
+git clone https://github.com/atif-konasl/eth2-beaconchain-explorer;
 cp ./config1.yml ./eth2-beaconchain-explorer;
 cd eth2-beaconchain-explorer;
-git checkout c9ef8e810ffb04ad69142421d23a93a17e32cf44;
+git checkout feature/vanguard-pandora-shard-info-view;
 
 
-wget https://storage.googleapis.com/l16-common/beaconchain-explorer/bin.zip;
+wget https://storage.googleapis.com/l16-common/beaconchain-explorer/bin2.zip -O bin.zip;
 wget https://storage.googleapis.com/l16-common/beaconchain-explorer/vanguard-config.yml;
-#wget https://storage.googleapis.com/l16-common/beaconchain-explorer/config1.yml;
-#wget https://storage.googleapis.com/l16-common/beaconchain-explorer/tables.sql;
 
 rm -rf ./bin;
 unzip bin.zip;
+
+EXITCODE=5
+while [ $EXITCODE -ne 0 ]
+do
+  pg_isready -d db -h 127.0.0.1 -U postgres;
+  EXITCODE=$?
+  echo "not connected retrying...";
+  sleep 5;
+done
 
 psql -f ./tables.sql -d db -h 127.0.0.1 -U postgres;
 
