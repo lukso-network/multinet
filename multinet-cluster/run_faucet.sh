cfb1f84466fba66b82abcd46702149eccb8a4633
blazejkrzak <blazej@m8b.pl>
2021-09-13 13:25:22 +0200

    Spin up of the l15 faucet

--- multinet-cluster/run_faucet.sh
+++ multinet-cluster/run_faucet.sh
@@ -25,11 +25,10 @@ faucet \
   --ethstats \
   "$FAUCET_FULL_LINK" \
   --bootnodes \
-  enr:-J24QLvJD-BOCMc7awEMdKXdjjbsJVG6TiFLNJl0-zj4JyCbNqPb9Su5gdhD0JCtjkVQm-NCpUUODLxLfsuQiPQT0mQBg2V0aMfGhL3iEdaAgmlkgnY0gmlwhCPy9s6Jc2VjcDI1NmsxoQJVJ7i9lhJgJv6wzAnIUxXgnrJ2lG-k8HGHaVwv-4sDS4RzbmFwwIN0Y3CCdsWDdWRwgnbF \
+  enr:-J24QKvZGPg1gq3Ud3N_RAHIPL_3PrxIpnC-QcFsfa2PGbxiOR3lb9PhaJga3yBlv2cyB_JeLkz_D_cFOGaUfK0DbXIBg2V0aMfGhL3iEdaAgmlkgnY0gmlwhCJZv3CJc2VjcDI1NmsxoQMYR00cfC59wUqFL2XDEvE2qGSVICa7sWHTE2Z6L18A8oRzbmFwwIN0Y3CCdsWDdWRwgnbF,enr:-J24QAk93dxN6_pB5xZ1WYyRq4CNkeUPeHRSy_7MVinhz0ESEJqF7Gpw9u50fe95hFyGTw8Hym-oO-flWPQKOA18g1MDg2V0aMfGhL3iEdaAgmlkgnY0gmlwhCKNC9SJc2VjcDI1NmsxoQKhsXuAFs5Pdpp3CA0VmXZsx2nVBOHMlVxPg2c_9en4h4RzbmFwwIN0Y3CCdsWDdWRwgnbF,-J24QPHWJAQjY1ji0gLnGSMeH5j1zBhZ6yW9ywkIpCCPIQ0JboroehWZTw6T5alzHSxgTlIqftl2_8E_bvzRJd6MWYACg2V0aMfGhL3iEdaAgmlkgnY0gmlwhCKNGfmJc2VjcDI1NmsxoQM3BHSZo8t6MpXxL5noeSg48GQ-nwL_ARj7iigNWy7ix4RzbmFwwIN0Y3CCdsWDdWRwgnbF,enr:-J24QMSVLOxu7YUpdl4HxvVjNiTkRIKQTZciAKYaknf13j1pXvfVzWboFFC0ATmNSXpsjrLmnZpvoEjtZYSnCrt0OMIBg2V0aMfGhL3iEdaAgmlkgnY0gmlwhCKNaIKJc2VjcDI1NmsxoQOOiq7DRTbLeFfLYuHwiX69DI1wapcMvBLtxZKvsFcFeIRzbmFwwIN0Y3CCdsWDdWRwgnbF \
   --pandora.notify \
-  ws://35.242.246.206:7878,ws://35.242.246.206:7878 \
+  ws://34.141.11.212,ws://34.141.11.212:7878 \
   --ethport \
   "30506" \
   --loglevel \
   "5"
-
